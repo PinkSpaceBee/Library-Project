cf1208ceea7ce617421f141c449ebd07094d24af
PinkSpaceBee <minaary@mailfence.com>
2021-12-01 18:19:34 +0200

    add function to store new books and a form for submitting a new book

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
 const library = [];
 
-function Book(title, author, pagesTotal, haveRead) {
-    this.title = title;
-    this.author = author;
-    this.pagesTotal = pagesTotal;
-    this.haveRead = haveRead;
+function Book() {
+    this.title = document.querySelector('#js-title').value;
+    this.author = document.querySelector('#js-author').value;
+    this.pagesTotal = document.querySelector('#js-pages-total').value;
+    this.haveRead = document.querySelector('#js-have-read').value;
 }
 
 Book.prototype.getInfo = function() {
     return `${this.title} by ${this.author}, ${this.pagesTotal} pages, ${this.haveRead ? 'read' : 'not read yet'}`;
 };
 
+function test() {
 
+    library.push(new Book(this.title, this.author, this.pagesTotal, this.haveRead));
+}
+
+const addBookBtn = document.querySelector('#js-add-btn');
+const submitBookCard = document.querySelector('#js-submit-book-card');
+
+addBookBtn.addEventListener('click', () => {
+});
+
+
+submitBookCard.addEventListener('click', () => {
+    test();
+
+    const inputFields = Array.from(document.querySelector('#js-new-book-card').querySelectorAll('input'));
+    inputFields.forEach(i => i.type !== 'submit' ? i.value = '' : i);
+    document.querySelector('#js-have-read').checked = false;
+});
