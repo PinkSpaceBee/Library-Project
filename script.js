d5054e6c5dfe84af77777f7bba60dba9f0cf3431
PinkSpaceBee <minaary@mailfence.com>
2021-12-01 13:44:15 +0200

    add Book constructor

--- script.js
+++ script.js
@@ -3,34 +3,10 @@ function Book(title, author, pagesTotal, haveRead) {
     this.author = author;
     this.pagesTotal = pagesTotal;
     this.haveRead = haveRead;
-
-    this.getInfo = function() {
-        return `${title} by ${author}, ${pagesTotal} pages, ${haveRead ? 'read' : 'not read yet'}`;
-    }
-
-    this.isGood = 'almost certainly';
 }
 
-const obj = {
-    test: true,
-}
-
-Object.setPrototypeOf(Book, obj);
-
-
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', true);
-
-const theMartianChronicles = {};
-Object.setPrototypeOf(theMartianChronicles, Book.prototype);
-
-const blindsight = {
-    __proto__: Book,
+Book.prototype.getInfo = function() {
+    return `${this.title} by ${this.author}, ${this.pagesTotal} pages, ${this.haveRead ? 'read' : 'not read yet'}`;
 };
 
-const roadsidePicnic = {
-    __proto__: Book.prototype,
-}
 
-const hisMartersVoice = {
-    __proto__: obj,
-}
